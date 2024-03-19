0c9b3d9e1bfec895b41e4d9b7dc09e69dc2c5829
Stanislav <spotes@yandex.ru>
2024-03-19 20:06:48 +0300

    Task_4

---
+++ task_4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает натуральное число N,
+//  а на выходе показывает его цифры через запятую.
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int N;
+        while (!Int32.TryParse(Console.ReadLine(), out N))
+        {
+            Console.WriteLine("Введите целое число:");
+        }
+
+        string result = string.Join(",", N.ToString().Select(c => c - '0'));
+        Console.WriteLine(result);
+    }
+}
