740f03333f63173a2f5d1b732d0a0f8691bea33c
Stanislav <spotes@yandex.ru>
2024-03-19 19:59:29 +0300

    Task_3

---
+++ task_1/task_2/task_3/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 3: Напишите программу, которая принимает на вход целое число из отрезка [10, 99] 
+//и показывает наибольшую цифру числа.
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите число в диапазоне от 10 до 99:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number >= 10 && number <= 99)
+        {
+            string numberString = number.ToString();
+            int maxDigit = 0;
+
+            foreach (char digit in numberString)
+            {
+                int digitValue = Convert.ToInt32(digit.ToString());
+                if (digitValue > maxDigit)
+                {
+                    maxDigit = digitValue;
+                }
+            }
+
+            Console.WriteLine("Наибольшая цифра числа: " + maxDigit);
+        }
+        else
+        {
+            Console.WriteLine("Введенное число не находится в диапазоне от 10 до 99.");
+        }
+    }
+}
